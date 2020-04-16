77a47f3cb12b3880dfa2f14de7875f12fa83203e
Alexander Garuba <alexandergaruba96@gmail.com>
2020-04-16 23:23:34 +0100

    Add about text

--- src/app.js
+++ src/app.js
@@ -24,14 +24,14 @@ app.use(express.static(publicDirectoryPath))
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
-        name: 'Andrew Mead'
+        name: 'Alexander Garuba'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
-        name: 'Andrew Mead'
+        name: 'Alexander Garuba'
     })
 })
 
@@ -39,7 +39,7 @@ app.get('/help', (req, res) => {
     res.render('help', {
         helpText: 'This is some helpful text.',
         title: 'Help',
-        name: 'Andrew Mead'
+        name: 'Alexander Garuba'
     })
 })
 
@@ -94,7 +94,7 @@ app.get('/products', (req, res) => {
 app.get('/help/*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'Alexander Garuba',
         errorMessage: 'Help article not found.'
     })
 })
@@ -102,7 +102,7 @@ app.get('/help/*', (req, res) => {
 app.get('*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'Alexander Garuba',
         errorMessage: 'Page not found.'
     })
 })
